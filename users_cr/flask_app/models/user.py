44523bc20eb9071ffebf5025e9941d0cfa1c5af9
Dalius Beck <41458059+DaliusBeckjr@users.noreply.github.com>
2023-05-05 14:43:41 -0700

    Update user.py
    just wanted to add a choose one classmethod example for the rest of the crud assignment/ project

--- users_cr/flask_app/models/user.py
+++ users_cr/flask_app/models/user.py
@@ -28,6 +28,16 @@ class User:
             # need to pass in a dict to fill out info for data
             all_users.append(cls(user))
         return all_users
+   
+# the get_one method will be used when we need to retrieve just one specific row of the table.
+# just added this...now have to implement it with the following code... and add update class 
+# and delete class for the rest of the CRUD method but this is apart of the crus method: read
+    @classmethod
+    def get_one(cls, user_id):
+        query  = "SELECT * FROM friends WHERE id = %(id)s";
+        data = {'id':user_id}
+        results = connectToMySQL(cls.db).query_db(query, data)
+        return cls(results[0])
     
 # crud method: create
     @classmethod
